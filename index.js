940c8c632f2aa822aa50517a4726aa0b8bf67578
xingchen <xingchen.zhao@huimeihealth.com.cn>
2022-11-07 18:05:36 +0800

    完成命令行交互功能

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import { generate } from "./lib/generator.js";
 import { createRandomPicker } from "./lib/random.js";
 import { loadCorpus, saveCorpus } from "./lib/corpus.js";
+import { options } from "./lib/cmd.js"
 
+//获取JSON
 const corpus = loadCorpus("corpus/data.json")
-
-const pickTitle = createRandomPicker(corpus.title)
-const title = pickTitle()
-
-const article = generate(title, {corpus})
-
+//获取title
+const title = options.title || createRandomPicker(corpus.title)()
+//生成文章
+const article = generate(title, {corpus, ...options})
+//保存文章
 saveCorpus(title, article)
